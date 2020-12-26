b00c3dd5420019f73cc291efde4b415b0b761788
Cihan İçelliler <icellilercihan@gmail.com>
2020-12-27 02:14:37 +0300

    added keyframes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const tl=gsap.timeline({default:{ease:"power1.out"}});
 
 tl.to(".text",{y:"0%",duration:1,stagger:0.3});
@@ -5,3 +6,4 @@ tl.to(".slider",{y:"-100%",duration:1.5,delay:3});
 tl.to(".intro",{y:"-100%",duration:1},"-=1");
 tl.fromTo("nav",{opacity:0},{opacity:1,duration:1.5});
 tl.fromTo(".big-text",{opacity:0},{opacity:1,duration:1.5},"-=1");
+
