4267d08c7eae681986ec2173f0ffcd91a8c36371
Cihan İçelliler <icellilercihan@gmail.com>
2020-12-26 19:40:49 +0300

    slider1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const tl=gsap.timeline({default:{ease:"power1.out"}});
 
-tl.to(".text",{y:"0%",duration:1,stagger:0.25});
-tl.to(".slider",{y:"-100%",duration:1.1});
+tl.to(".text",{y:"0%",duration:1,stagger:0.3});
+tl.to(".slider",{y:"-100%",duration:1.5,delay:3});
 tl.to(".intro",{y:"-100%",duration:1},"-=1");
-tl.fromTo("nav",{opacity:0},{opacity:1,duration:1.2});
-tl.fromTo(".big-text",{opacity:0},{opacity:1,duration:1.2},"-=1");
+tl.fromTo("nav",{opacity:0},{opacity:1,duration:1.5});
+tl.fromTo(".big-text",{opacity:0},{opacity:1,duration:1.5},"-=1");
