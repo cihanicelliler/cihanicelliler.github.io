5a79aeb2ab6a23c04ac4d672a4735dd7a538650c
Cihan İçelliler <icellilercihan@gmail.com>
2021-01-07 01:04:29 +0300

    adding animation

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ tl.to(".intro",{y:"-100%",duration:1},"-=1");
 tl.fromTo("nav",{opacity:0},{opacity:1,duration:1.5});
 tl.fromTo(".big-text",{opacity:0},{opacity:1,duration:1.5},"-=1");
 
+const svg= document.querySelectorAll('#svg path');
+for(let i=0;i<svg.length;i++){
+    console.log(`letter ${i} is ${svg[i].getTotalLength()}`);
+}
